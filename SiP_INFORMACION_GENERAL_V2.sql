2f3946a14a74d4089cc9c743ba359cd5bd5ae9f8
DavidGuerreroL <86793270+DavidGuerreroL@users.noreply.github.com>
2025-07-03 12:45:45 -0600

    45394
    EAC - Se eliminan lineas comentadas

--- SiP_INFORMACION_GENERAL_V2.sql
+++ SiP_INFORMACION_GENERAL_V2.sql
@@ -6,28 +6,5 @@
 AS
 
 BEGIN
-
---IF V_GRUPO = '1' THEN
-      open cv_1 for Select * From Vi_Informacion_g1 Where pr_mc_expediente= V_EXPEDIENTE;
-/* ELSIF V_GRUPO = '2' THEN
-      open cv_1 for Select * From Vi_Informacion_g2 Where pr_mc_expediente= V_EX
-PEDIENTE;
-ELSIF V_GRUPO = '3' THEN
-      open cv_1 for Select * From Vi_Informacion_g3 Where pr_mc_expediente= V_EX
-PEDIENTE;
-ELSIF V_GRUPO = '4' THEN
-      open cv_1 for Select * From Vi_Informacion_g4 Where pr_mc_expediente= V_EX
-PEDIENTE;
-ELSIF V_GRUPO = '5' THEN
-      open cv_1 for Select * From Vi_Informacion_g5 Where pr_mc_expediente= V_EX
-PEDIENTE;
-ELSIF V_GRUPO = '6' THEN
-      open cv_1 for Select * From Vi_Informacion_g6 Where pr_mc_expediente= V_EX
-PEDIENTE;
-ELSIF V_GRUPO = '8' THEN
-      open cv_1 for Select * From Vi_Informacion_g8 Where pr_mc_expediente= V_EX
-PEDIENTE;
-END IF;*/
-
-
+     open cv_1 for Select * From Vi_Informacion_g1 Where pr_mc_expediente= V_EXPEDIENTE;
 END;
